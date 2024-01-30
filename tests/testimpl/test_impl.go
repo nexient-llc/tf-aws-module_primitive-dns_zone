dc4e89309f85e61b82e902b6d9cd41210a398625
nexient-ajoshi <97111743+nexient-ajoshi@users.noreply.github.com>
2024-01-30 11:15:43 -0500

    Fix terratests to match the new output values (#8)
    Co-authored-by: Aarti Joshi <aarti.joshi@nttdata.com>

--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -10,32 +10,27 @@ import (
 )
 
 func TestComposableComplete(t *testing.T, ctx types.TestContext) {
-
 	t.Run("TestIsZoneExist", func(t *testing.T) {
-		// Please fix this test, zone_id no longer exists as a valid output
-		t.SkipNow()
-		zone_id := terraform.Output(t, ctx.TerratestTerraformOptions, "zone_id")
-		expected_zone_name := ctx.TestConfig.(*ThisTFModuleConfig).Zone_name
-		zone := dns.GetHostedZoneById(t, zone_id)
-		assert.Equal(t, *zone.HostedZone.Name, dns.NameNormalize(expected_zone_name))
+		zone_ids := terraform.OutputMap(t, ctx.TerratestTerraformOptions, "route53_zone_zone_ids")
+		for zone_name := range zone_ids {
+			expected_zone_name := ctx.TestConfig.(*ThisTFModuleConfig).Zones[zone_name].Domain_name
+			zone := dns.GetHostedZoneById(t, zone_ids[zone_name])
+			assert.Equal(t, *zone.HostedZone.Name, dns.NameNormalize(expected_zone_name))
+		}
 	})
 }
 
 func TestNonComposableComplete(t *testing.T, ctx types.TestContext) {
-	if !ctx.IsCurrentTest("public_zone") {
-		t.SkipNow()
-	}
 	t.Run("TestIsRecordExists", func(t *testing.T) {
 		if !testDataHaveDNSRecords(t, ctx) {
-			t.SkipNow()
-		}
-		// Please fix this test, zone_id no longer exists as a valid output
-		t.SkipNow()
-		zone_id := terraform.Output(t, ctx.TerratestTerraformOptions, "zone_id")
-		for _, rec := range ctx.TestConfig.(*ThisTFModuleConfig).Records {
-			fullQualifiedRecordName := rec.Name + "." + dns.NameNormalize(ctx.TestConfig.(*ThisTFModuleConfig).Zone_name)
-			_, err := dns.LookupDNSRecordInPublicRoute53ZoneByDNSProtocol(t, zone_id, fullQualifiedRecordName, rec.Type)
-			assert.NoError(t, err, "can not find expected DNS record in AWS DNS: "+fullQualifiedRecordName)
+			zone_ids := terraform.OutputMap(t, ctx.TerratestTerraformOptions, "route53_zone_zone_ids")
+			for zone_name := range zone_ids {
+				for _, rec := range ctx.TestConfig.(*ThisTFModuleConfig).Records {
+					fullQualifiedRecordName := rec.Name + "." + dns.NameNormalize(ctx.TestConfig.(*ThisTFModuleConfig).Zones[zone_name].Domain_name)
+					_, err := dns.LookupDNSRecordInPublicRoute53ZoneByDNSProtocol(t, zone_ids[zone_name], fullQualifiedRecordName, rec.Type)
+					assert.NoError(t, err, "can not find expected DNS record in AWS DNS: "+fullQualifiedRecordName)
+				}
+			}
 		}
 
 	})
